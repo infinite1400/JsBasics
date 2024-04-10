15a5b6b4e2ef11ddc71718ee2d8262b7b45f4b96
infinite1400 <muraripandey1886@gmail.com>
2024-04-10 19:03:23 +0530

    added CRUD Model functions

--- Node_tut_TS/src/db/users.ts
+++ Node_tut_TS/src/db/users.ts
@@ -1,29 +1,44 @@
 import mongoose from "mongoose";
 
-const UserSchema=new mongoose.Schema({
-    username : { type :String , required : true},
-    email : { type : String , required : true},
-    authentication : {
-        password : {type : String,required : true , select : false},
-        salt : { type : String , select : false},
-        sessionToken : { type : String, select : false}
-    }
-})
-const UserModel=mongoose.model("User",UserSchema);
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  authentication: {
+    password: { type: String, required: true, select: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
+  },
+});
+const UserModel = mongoose.model("User", UserSchema);
 export default UserModel;
 
-export const getUsers=()=> UserModel.find();
+// functions !
+export const getUsers = () => {
+  return UserModel.find();
+};
 
-export const getUserByEmail=(email : string) => UserModel.findOne({email : email})
+export const getUserByEmail = (email: string) => {
+  return UserModel.findOne({ email: email });
+};
 
-export const getUserBySessionToken=(sessionToken : string) => UserModel.findOne({
-    'authentication.sessionToken': sessionToken
-});
+export const getUserBySessionToken = (sessionToken: string) => {
+  return UserModel.findOne({
+    "authentication.sessionToken": sessionToken,
+  });
+};
 
-export const getUserById=(id : string) =>UserModel.findById(id)
+export const getUserById = (id: string) => {
+  return UserModel.findById(id);
+};
 
-export const createUser = (values : Record<string,any>) => new UserModel(values).save().then((user)=>user.toObject());
+export const createUser = (values: Record<string, any>) => {
+  return new UserModel(values).save().then((user) => user.toObject());
+};
 
-export const deleteUserModel=(id : string) => UserModel.findOneAndDelete({_id : id});
+export const deleteUserModel = (id: string) => {
+  return UserModel.findByIdAndDelete(id);
+};
 
-const updateUserById=(id : string, values : Record<string,any>) => UserModel.findByIdAndUpdate(id,values);
+const updateUserById = (id: string, values: Record<string, any>) => {
+  return UserModel.findByIdAndUpdate(id, values);
+};
